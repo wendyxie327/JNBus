ab986802d5f0910e993a62c0718419efe7abea3e
wendyxie <wendyxie327@163.com>
2017-01-15 21:17:31 +0800

    添加更新连接微步IP地址功能；添加GitHub显示功能

--- JNBus/app/src/main/java/com/wendy/jnbus/ui/fragment/SettingsFragment.java
+++ JNBus/app/src/main/java/com/wendy/jnbus/ui/fragment/SettingsFragment.java
@@ -13,6 +13,7 @@ import com.wendy.jnbus.R;
 import com.wendy.jnbus.net.NoAddressHttpMethod;
 import com.wendy.jnbus.persistence.BusShare;
 import com.wendy.jnbus.ui.JNBusApplication;
+import com.wendy.jnbus.vo.Address;
 import com.wendy.jnbus.vo.Version;
 
 /**
@@ -28,39 +29,80 @@ public class SettingsFragment extends PreferenceFragment {
         addPreferencesFromResource(R.xml.preference_settings);
 
 
+        //显示当前版本号
         Preference versionPref = findPreference("app_version");
         versionPref.setSummary(AppUtil.getVersionName(JNBusApplication.getContext()));
 
+        //更新IP地址
         Preference setUpdatePref = findPreference("set_update");
         setUpdatePref.setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
             @Override
             public boolean onPreferenceClick(Preference preference) {
-                NoAddressHttpMethod.getInstance().checkLastestVersion(getActivity(), new SubscriberOnNextListener<Version>() {
-                    @Override
-                    public void onNext(Version version) {
-                        Logger.d(TAG,"version="+version.toString());
-                        final MaterialDialog dialog = new MaterialDialog(getActivity());
-                        dialog.setPositiveButton(R.string.dialog_sure, new View.OnClickListener() {
-                            @Override
-                            public void onClick(View v) {
-                                dialog.dismiss();
-                            }
-                        });
-
-                        if ( BusShare.getKeyVersionCode() == version.getVersionCode()){
-                            dialog.setMessage(getString(R.string.pref_set_update_dialog_lastest)+ version.getVersionCode());
-                        }else {
-                            BusShare.setKeyVersionCode(version.getVersionCode());
-                            dialog.setMessage(getString(R.string.pref_set_update_dialog_change_left)+ version.getVersionCode());
-                        }
-                        dialog.show();
-                    }
-                });
+                NoAddressHttpMethod.getInstance().checkLastestVersion(getActivity(), getVersionListener());
                 return false;
             }
         });
     }
 
+    /**
+     * 获取微步最新版本号，并进行设置
+     * @return
+     */
+    private SubscriberOnNextListener<Version> getVersionListener(){
+        SubscriberOnNextListener<Version> onNextListener = new SubscriberOnNextListener<Version>() {
+            @Override
+            public void onNext(Version version) {
+                Logger.d(TAG,"version="+version.toString());
+                final MaterialDialog dialog = new MaterialDialog(getActivity());
+                dialog.setPositiveButton(R.string.dialog_sure, new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        dialog.dismiss();
+                        NoAddressHttpMethod.getInstance().checkIP(getActivity(),getStatusListener());
+                    }
+                });
 
+                if ( BusShare.getKeyVersionCode() == version.getVersionCode()){
+                    dialog.setMessage(getString(R.string.pref_set_update_dialog_lastest)+ version.getVersionCode());
+                }else {
+                    BusShare.setKeyVersionCode(version.getVersionCode());
+                    dialog.setMessage(getString(R.string.pref_set_update_dialog_change_left)+ version.getVersionCode());
+                }
+                dialog.show();
+            }
+        };
+        return onNextListener;
+    }
+
+    /**
+     * 更新IP地址
+     * @return
+     */
+    private SubscriberOnNextListener<Address> getStatusListener(){
+        SubscriberOnNextListener<Address> onNextListener = new SubscriberOnNextListener<Address>() {
+            @Override
+            public void onNext(Address address) {
+                Logger.d(TAG,"address="+address.toString());
+                final MaterialDialog dialog = new MaterialDialog(getActivity());
+                dialog.setPositiveButton(R.string.dialog_sure, new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        dialog.dismiss();
+                    }
+                });
+                if ( address!=null){
+                    BusShare.setKeyBusIpAndArea(address.getHttpAddr(), address.getName());
+                    if (BusShare.getKeyBusIp().equals(address.getHttpAddr())){
+                        dialog.setMessage(getString(R.string.pref_set_update_ip_dialog_lastest)+ address.getHttpAddr());
+                    }else {
+                        dialog.setMessage(getString(R.string.pref_set_update_ip_dialog_change_left)+ address.getHttpAddr());
+                    }
+                    dialog.show();
+                }
+
+            }
+        };
+        return onNextListener;
+    }
 
 }
