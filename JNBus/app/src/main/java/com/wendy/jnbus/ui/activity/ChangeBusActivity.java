2396afdbf2571f20aec2739dde14ace3e80b2ef0
wendyxie <wendyxie327@163.com>
2017-08-17 22:17:06 +0800

    换乘页面，添加定位功能 TODO 添加6.0系统的支持

--- JNBus/app/src/main/java/com/wendy/jnbus/ui/activity/ChangeBusActivity.java
+++ JNBus/app/src/main/java/com/wendy/jnbus/ui/activity/ChangeBusActivity.java
@@ -6,19 +6,28 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.ListView;
 
+import com.baidu.location.BDLocation;
+import com.baidu.location.BDLocationListener;
+import com.baidu.location.LocationClient;
+import com.eagle.androidlib.utils.Logger;
 import com.wendy.jnbus.R;
 import com.wendy.jnbus.ui.base.BaseAppActivity;
+import com.wendy.jnbus.util.baidumap.MapUtil;
 
 import butterknife.OnClick;
 
 /**
  * 车辆换乘
- *
+ * <p>
  * 需要完成的功能：定位/查询地址/展示换乘方案/查询地址反转/查询历史地点记录
  * Created by Wendy on 2017/7/27.
  */
 
-public class ChangeBusActivity extends BaseAppActivity {
+public class ChangeBusActivity extends BaseAppActivity implements BDLocationListener{
+
+    private LocationClient mLocationClient = null;
+    private MapUtil mapUtil;
+
     @Override
     public int getLayoutID() {
         return R.layout.activity_change_bus;
@@ -31,7 +40,10 @@ public class ChangeBusActivity extends BaseAppActivity {
 
     @Override
     public void initDataCreate() {
-
+        mLocationClient = new LocationClient(getApplicationContext());
+        mapUtil = new MapUtil();
+        mapUtil.startLocationOnce(this, mLocationClient);
+        //TODO 适配6.0系统以上
     }
 
     @Override
@@ -40,9 +52,15 @@ public class ChangeBusActivity extends BaseAppActivity {
     }
 
     @OnClick(R.id.from_et)
-    public void clickFromAddress(){
+    public void clickFromAddress() {
         startActivity(new Intent(this, SearchLocationActivity.class));
     }
 
 
+    @Override
+    public void onReceiveLocation(BDLocation bdLocation) {
+        if ( bdLocation!= null){
+            Logger.d(context, " location = "+bdLocation.getAddrStr());
+        }
+    }
 }
--- JNBus/app/src/main/java/com/wendy/jnbus/ui/activity/SearchLocationActivity.java
+++ JNBus/app/src/main/java/com/wendy/jnbus/ui/activity/SearchLocationActivity.java
@@ -1,18 +1,27 @@
 package com.wendy.jnbus.ui.activity;
 
-import android.app.SearchManager;
-import android.content.Intent;
-import android.widget.Toast;
+import android.os.Bundle;
+import android.widget.ListView;
+import android.widget.SearchView;
 
 import com.eagle.androidlib.utils.Logger;
 import com.wendy.jnbus.R;
 import com.wendy.jnbus.ui.base.BaseAppActivity;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
 /**
  * Created by Wendy on 2017/8/12.
+ * 查询地点
  */
 
 public class SearchLocationActivity extends BaseAppActivity {
+    @BindView(R.id.search_view)
+    SearchView searchView;
+    @BindView(R.id.list_lv)
+    ListView listLv;
+
     @Override
     public int getLayoutID() {
         return R.layout.activity_search_location;
@@ -25,7 +34,7 @@ public class SearchLocationActivity extends BaseAppActivity {
 
     @Override
     public void initDataCreate() {
-        Logger.d(context, "ini");
+
     }
 
     @Override
